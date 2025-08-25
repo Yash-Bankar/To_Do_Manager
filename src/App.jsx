cc28b4364f312293441e3887e8fde24787528c7d
Yash Bankar <anonyallrounder@gmail.com>
2025-08-25 21:16:20 +0530

    Refactor App component and clean up code

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ import Navbar from './Navbar'
 import dustbinIcon from './assets/dustbin-black.svg';
 import penIcon from './assets/pen-black.svg';
 
-
 function App() {
-  const [count, setCount] = useState(0)
   const logo = "AOTasks"
   const obj = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : [];
   const [todo, setTodo] = useState("");
@@ -33,15 +31,9 @@ function App() {
     localStorage.setItem("todos", JSON.stringify(newObj));
   }
 
-  // const handleDoneTasks = () => {
-  //   const newObj = todos.filter((item) => item.isCompleted);
-  //   setTodos(newObj);
-  //   localStorage.setItem("todos", JSON.stringify(newObj));
-  // }
 
 
   const handleAdd = () => {
-    // also fetch from local storage
     const newObj = todos.map((item, i) => i === editIndex ? { ...item, todo } : item);
     if (editIndex !== null) {
       setTodos(newObj);
@@ -69,7 +61,7 @@ function App() {
     <>
     <Navbar logo={logo} />
 
-  <div className="container mx-auto flex flex-col items-center min-h-[70vh] p-4 border border-green-500 rounded-3xl mt-10 bg-[#1a1a1a]">
+  <div className="container w-[100vw] md:w-[70vw] mx-auto flex flex-col items-center min-h-[70vh] p-4 border border-green-500 rounded-3xl mt-10 bg-[#1a1a1a]">
       <div className="addtodo text-green-500 ">
         <h1 className="text-4xl font-bold typewriter">Add to-do's</h1>
           <input 
@@ -81,10 +73,11 @@ function App() {
           />
           <button onClick={handleAdd} disabled={todo.length<1} className="bg-green-500 text-black rounded-full cursor-pointer px-4 py-2 mt-4">{editIndex !== null ? 'Update' : 'Add'}</button>
       </div>
-      <div className="taskhead text-green-500 flex justify-between items-center w-[50vw] mt-10" ><div className=" text-2xl  font-bold">Your tasks:</div>
-      <div><input type="checkbox" className={` mt-3 rounded focus:ring-2 focus:ring-green-400 border-green-500 accent-green-500 bg-green-900' `}
-                  checked={showfinished}
-                  onChange={togglefinished} /> Show completed</div>
+      <div className="taskhead text-green-500 flex flex-col justify-center items-center md:flex-row md:justify-between md:items-center w-full md:w-[50vw] mt-10">
+        <div className=" text-2xl  font-bold">Your tasks:</div>
+        <div><input type="checkbox" className={` mt-3 rounded focus:ring-2 focus:ring-green-400 border-green-500 accent-green-500 bg-green-900' `}
+                    checked={showfinished}
+                    onChange={togglefinished} /> Show completed</div>
       </div>
 
  <div className="todos mt-4">
@@ -93,7 +86,8 @@ function App() {
         ) : (
           todos.filter(item => showfinished || !item.isCompleted)
           .map((item, index) => (
-            <div key={index} className="todo text-green-500 border border-green-500 rounded-full p-4 mb-4 flex justify-between items-center w-[60vw]">
+            <div key={index} className="todo text-green-500 border border-green-500 rounded-4xl md:rounded-full p-4 mb-4 flex flex-col md:flex-row justify-between items-start md:items-center w-[80vw] md:w-[60vw]">
+
               <div className={item.isCompleted ? "line-through decoration-black" : ""}>{item.todo}</div>
               <div className="buts flex gap-3 justify-center items-center">
                 <input
