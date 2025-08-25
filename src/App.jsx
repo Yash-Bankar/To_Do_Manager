c8853338f93892c93db934a0994a04b8af20e38c
Yash Bankar <anonyallrounder@gmail.com>
2025-08-25 20:45:34 +0530

    Remove unused imports from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './Navbar'
 import dustbinIcon from './assets/dustbin-black.svg';
 import penIcon from './assets/pen-black.svg';
-import { set } from 'mongoose'
+
 
 function App() {
   const [count, setCount] = useState(0)
